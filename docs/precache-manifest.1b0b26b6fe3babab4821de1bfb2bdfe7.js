ab2a848773a1b018d33428a9e7bf222e87c1c322
hugo lazzari <37092379+katyushi@users.noreply.github.com>
2021-03-09 11:35:36 -0300

    first build

---
+++ docs/precache-manifest.1b0b26b6fe3babab4821de1bfb2bdfe7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "534d349f0703c6bfd2c6",
+    "url": "/css/app.35892048.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "178619e27fbce75bbe12efef0b425910",
+    "url": "/index.html"
+  },
+  {
+    "revision": "06f4162b0dd322c7727b",
+    "url": "/js/about-legacy.2947b6ea.js"
+  },
+  {
+    "revision": "534d349f0703c6bfd2c6",
+    "url": "/js/app-legacy.e339ed07.js"
+  },
+  {
+    "revision": "f077cadcb110dc8da505",
+    "url": "/js/chunk-vendors-legacy.fc010b69.js"
+  },
+  {
+    "revision": "69b649f3f7f4f8f6e1b3afed797a8459",
+    "url": "/manifest.json"
+  }
+]);
